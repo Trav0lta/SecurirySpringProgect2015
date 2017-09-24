9b071078a04e8f8cce190bebe6d8a091e22b27dc
Trav0lta <lloginoleg123@gmail.com>
2017-09-24 12:36:48 +0300

    херово закокімітив минулого разу тому перезаливаю зі старим коментом)

--- securirySpring/src/main/java/ua/com/owu/dao/UserDAO.java
+++ securirySpring/src/main/java/ua/com/owu/dao/UserDAO.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ua.com.owu.entity.User;
 
+
 public interface UserDAO extends JpaRepository<User, Integer> {
     @Query("from User u where u.username=:username")
     public User findByUserName(@Param("username") String username);
+
 }
